6038dfc0a55402c0b5cf2638ba7972d207a255bc
bhowe111 <brendan.howe1@gmail.com>
2020-12-10 14:45:40 -0800

    created routes

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import ProjectsContainer from './containers/ProjectsContainer';
+
+// Container
+import ProjectsContainer from '../containers/ProjectsContainer';
 
 // Pages
-import Home from './pages/Home';
-import About from './pages/About';
-import Projects from './pages/Projects';
+import Home from '../pages/Home';
+import About from '../pages/About';
+import Projects from '../pages/Projects';
+import Contact from '../pages/Contact';
 
 // Routes
+export default (
+  <Switch>
+    <Route exact path='/' component={ Home } />
+    <Route path='/about' component={ About } />
+    <Route path='/projects' component={ Projects } />
+    <Route path='/contact' component={ Contact } />
+  </Switch>
+)
