57cf35e2eb821ee56bcd0410898460a5196088d6
bhowe111 <brendan.howe1@gmail.com>
2020-12-10 15:44:06 -0800

    added basic css and updated project container

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
 import routes from './config/routes';
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -8,11 +7,9 @@ import Footer from './components/Footer';
 function App() {
   return (
     <div className="container">
-      <Switch>
         <Header />
         { routes }
         <Footer />
-      </Switch>
     </div>
   );
 }
