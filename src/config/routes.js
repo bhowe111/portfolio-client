78ad7ce5a6f53da7978a3b07c49c1a9c6d44be59
bhowe111 <brendan.howe1@gmail.com>
2020-12-10 14:35:09 -0800

    initial commit, created routes, pages, components, adding container

---
+++ src/config/routes.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import ProjectsContainer from './containers/ProjectsContainer';
+
+// Pages
+import Home from './pages/Home';
+import About from './pages/About';
+import Projects from './pages/Projects';
+
+// Routes
---
+++ src/containers/ProjectsContainer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class ProjectsContainer extends Component {
+  render() {
+    return (
+      <h2>Projects Container</h2>
+    )
+  }
+}
+
+export default ProjectsContainer;
